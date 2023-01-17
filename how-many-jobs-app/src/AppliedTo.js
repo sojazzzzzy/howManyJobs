b287e393af1b7bf638775b083376b31bddf08dd7
sojazzzzzy <jazmynewashington@gmail.com>
2023-01-17 09:52:19 -0800

    applied to div added

---
+++ how-many-jobs-app/src/AppliedTo.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import './index.css'
+
+class AppliedTo extends Component{
+   render() {
+    return (
+        <div class='appliedTo'>
+          <h1>applied to?</h1>  
+        </div>
+    )
+   }
+}
+
+export default AppliedTo
