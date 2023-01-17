405e61dd4c1d82aa9b1649ceb99bb91316b53e38
sojazzzzzy <jazmynewashington@gmail.com>
2023-01-17 09:58:46 -0800

    added goals to AppliedTo file

--- how-many-jobs-app/src/AppliedTo.js
+++ how-many-jobs-app/src/AppliedTo.js
@@ -1,14 +1,16 @@
-import React, {Component} from "react";
-import './index.css'
+import React, { Component } from 'react';
+import './index.css';
+// the idea is to have a viewable counter to the left of "applied to" that goes up everytime I click the button. The whole div can be a large button
+// every time I apply for a job, I click the button and update the counter
 
-class AppliedTo extends Component{
-   render() {
-    return (
-        <div class='appliedTo'>
-          <h1>applied to?</h1>  
-        </div>
-    )
-   }
+class AppliedTo extends Component {
+	render() {
+		return (
+			<div class='appliedTo'>
+				<h1>applied to?</h1>
+			</div>
+		);
+	}
 }
 
-export default AppliedTo
+export default AppliedTo;
